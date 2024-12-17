e5190e5f1823fc7f80f3fe8c171c5f6741259683
Polaris-algedi <karchalmohammed@gmail.com>
2024-12-17 21:28:35 +0100

    Add multiple instances of the button component

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,12 +1,28 @@
+import Button from "@/components/Button";
 import Card from "@/components/Card";
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
-      <Card />
-      <Card />
-      <Card />
+      <div className="md:grid grid-cols-3 gap-4">
+        <Card />
+        <Card />
+        <Card />
+
+        <Button
+          title="Button 1"
+          styles=" bg-blue-500 mt-3 px-3 py-1 text-white text-sm rounded-sm"
+        />
+        <Button
+          title="Button 2"
+          styles=" bg-blue-600 mt-3 px-4 py-2 text-white text-xl rounded-md"
+        />
+        <Button
+          title="Button 3"
+          styles=" bg-blue-700 mt-3 px-6 py-3 text-white text-4xl rounded-full"
+        />
+      </div>
     </div>
   );
 };
